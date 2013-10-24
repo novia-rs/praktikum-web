a866bd5ab6784794eb540354b5ffc09265c58faa
Novia Ratnasari <gamerz_anythings@yahoo.co.id>
2013-10-24 07:37:19 +0700

    Script PHP

---
+++ 110533406970/Praktikum ke-5/Tugas1/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Autentikasi Halaman</title>
+<style type="text/css">
+.inner{
+margin:200px auto; padding:20px; width:240px; border:1px solid #333;}
+</style>
+</head>
+<body>
+<p>
+  <?php
+ini_set('display_errors', 1);
+define('_VALID', 1);
+//include file eksternal
+require_once('./auth.php');
+init_login();
+validate();
+?>
+</p>
+<p align="right"><a href="?m=logout">Logout</a></p>
+<br/>
+<h3 align="center">Beranda Adminnya Kakak Cantik</h3>
+<h3 align="center">110533406970 | Novia Ratnasari </h3>
+<p align="center">
+<a href="?m=logout"></a>
+<p align="center"></p>
+
+Pada dasarnya cookie merupakan mekanisme untuk meletakkan data pada
+remote browser sehingga memudahkan penelusuran atau identifikasi user.
+Dapat juga dikatakan bahwa cookie merupakan informasi dalam bentuk teks
+yang dipertukarkan oleh client dan server, di mana pembuat cookie adalah
+pihak server.
+<br>
+Pada kenyataannya, penggunaan cookie memiliki beberapa masalah atau
+kendala. Bisa jadi ada browser yang tidak menerima cookie, serta
+kemungkinan adanya user yang men-disable cookie. Permasalahan ini
+merupakan salah satu alasan session PHP menggunakan method cookie/URL
+ganda.
+<br>
+<br>
+Thankyou ^_^
+17 Oktober 2013
+
+</body>
+</html>
